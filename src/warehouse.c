56472345b1ded915551b47827c550ceab94ee60f
cbermudez97 <cbermu.97@gmail.com>
2018-12-16 04:42:29 -0500

    Implementar inicialización del almacen
    Falta realizar pruebas mas rigurosas con el almacen para verificar
    que todo funcione correctamente.

--- src/warehouse.c
+++ src/warehouse.c
@@ -6,6 +6,16 @@
 
 Warehouse wh;
 
+void* process_client(void *raw_data) {
+    int clientfd = *((int*) raw_data);
+
+    //Falta parsear la peticion del cliente
+    ProductList request = new_productlist(10);//Solo para que compile
+    bool writing = false;//Indica si el cliente es productor o consumidor
+
+    return 0;
+}
+
 int main(int argc, char const *argv[]) {
     /*Prepare Warehouse Server*/
     int port = atoi(argv[1]);
@@ -13,6 +23,22 @@ int main(int argc, char const *argv[]) {
     /*End Preparing Server*/
     /*Inicialize Warehouse Structure*/
     wh.limit = atoi(argv[2]);
+    if (wh.limit == 0) {
+        wh.products = parseProductList((char**)(argv + 2));
+    }
+    else {
+        wh.products = parseProductList((char**)(argv + 3));
+    }
+    wh.limit = -1;
+    for (int i = 0; i < wh.products.size; ++i) {
+        Product p = index_productlist(&(wh.products), i);
+        remove_productlist(&(wh.products), i);
+        p.limit = p.count;
+        p.used = malloc(1 * sizeof(sem_t));
+        sem_init(p.used, 0, 1);
+        p.count = 0;
+        insert_productlist(&(wh.products), i, p);
+    }
     wh.act_cant = 0;
     //Falta iniciar la lista de productos de wh
     /*End Inicialization*/
@@ -23,12 +49,5 @@ int main(int argc, char const *argv[]) {
         pthread_t ct;
         pthread_create(&ct, NULL, &process_client, &clientfd);
     }
-    
     return 0;
-}
-
-void process_client(void *raw_data) {
-    int clientfd = *((int*) raw_data);
-    //Falta parsear la peticion del cliente
-    return;
 }
