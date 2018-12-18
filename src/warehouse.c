c076289e30e9e6f43336756b1d49f6b49cfffcb3
cbermudez97 <cbermu.97@gmail.com>
2018-12-18 13:10:13 -0500

    Cambiar logs

--- src/warehouse.c
+++ src/warehouse.c
@@ -31,6 +31,7 @@ void* process_client(void *raw_data) {
     if (req.type == PRODUCER)
         writing = true;
     else if (req.products.size == 0) {
+        printf("ANY\n");
         while(true) {
             for (int j = 0; j < get_size(&wh); ++j) {
                 Product *pw = indexref_productlist(&wh.products, j);
@@ -60,6 +61,7 @@ void* process_client(void *raw_data) {
                     // printf("Consumiendo producto de tipo %s.\n", pc.name);
                     product_remove(pw, &wh, &ret);
                     // printf("Consumido producto de tipo %s.\n", pc.name);
+                    printf("Devuelto %s %s %d %s\0", ret.name, ret.producer_name, ret.id, ret.data);
                     dprintf(clientfd,"%s %s %d %s\0", ret.name, ret.producer_name, ret.id, ret.data);
                     //free_product_data(&ret);
                 }
